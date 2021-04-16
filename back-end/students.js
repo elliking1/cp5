f3c2b0420a2a828ec1024998898ffdeaa29f7d92
elliking1 <71565785+elliking1@users.noreply.github.com>
2021-04-15 23:45:59 -0600

    Create students.js

---
+++ back-end/students.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const router = express.Router();
+const users = require("./users.js");
+const User = users.model;
+const validUser = users.valid;
+const groups = require("./groups.js");
+const Group = groups.model;
+
+// Create a model for contacts in the directory.
+const Student = mongoose.model('Student', studentSchema);
+
+// Create a new contact in the directory.
+app.post('/api/students', async (req, res) => {
+  const student = new Student({
+    groupNumber: req.body.groupNumber,
+    lastName: req.body.lastName,
+    firstName: req.body.firstName,
+    email: req.body.email,
+  });
+  try {
+    await student.save();
+    res.send(student);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/students', async (req, res) => {
+  try {
+    let students = await Student.find();
+    res.send(students);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/api/students/:id', async (req, res) => {
+  try {
+    await Student.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
