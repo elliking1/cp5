e6ef1296be85f50c9a892b0c5b05e363374d9b62
elliking1 <71565785+elliking1@users.noreply.github.com>
2021-04-15 23:52:24 -0600

    Update Home.vue

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,14 +1,94 @@
 <template>
-  <div class="home">
-  </div>
+<div class="home mainDirectory" >
+      <h2>Information</h2>
+      These study groups are completely optional, but a great way to ensure 
+      that you are retaining the information taught in class. Each study session 
+      will have at least one TA in attendance to answer questions. Extra credit will
+      be given to students who maintain at least 80% attendance in a study group. Sometime 
+      during the second week of the semester, you will be added to a discord group for 
+      your study group number.
+      <br><br>
+      <h2>Sign up for a Group</h2>
+      <div class="add">
+        <input v-model="groupNumber" placeholder="Study Group Number">
+        <input v-model="firstName" placeholder="Last Name">
+        <input v-model="lastName" placeholder="First Name">
+        <input v-model="email" placeholder="Email">
+        <button @click="add">Add Study Group</button>
+      </div>
+      <br><br>
+      <h2>Study Groups</h2>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Study Group #</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col"> </th>
+          </tr>
+        </thead>
+      <tbody>
+          <tr v-for="student in students" :key="student.id">
+            <th scope="row">{{student.groupNumber}}</th>
+            <td>{{student.lastName}}</td>
+            <td>{{student.firstName}}</td>
+            <td>{{student.email}}</td>
+            <td><button v-on:click="deleteStudent(student)" class="delete">X</button></td>
+          </tr>
+        </tbody>
+      </table>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import axios from 'axios';
 export default {
-  name: "Home",
-  components: {
+  name: 'Home',
+  data() {
+    return {
+     students: [],
+   }
   },
-};
+  created() {
+    this.getStudents();
+  },
+  methods: {
+    async getStudents() {
+      try {
+        let response = await axios.get("/api/students");
+        this.students = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async add() {
+      try {
+        await axios.post('/api/students', {
+          groupNumber: this.groupNumber,
+          lastName: this.lastName,
+          firstName: this.firstName,
+          email: this.email
+        });
+        this.groupNumber = "";
+        this.lastName = "";
+        this.firstName = "";
+        this.email = "";
+        this.getStudents();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async deleteStudent(student) {
+      try {
+        await axios.delete("/api/students/" + student._id);
+        this.getStudents();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+}
 </script>
